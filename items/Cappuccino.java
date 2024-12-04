8b36d3733679c14c1067bf9fd129979e40d0f745
mhdimo <akahito@naver.com>
2024-12-04 16:34:51 +0100

    Add Cappuccino class implementing Drink interface with pricing and amount management

---
+++ items/Cappuccino.java
@@ -0,0 +1,27 @@
+package items;
+
+import models.Cup;
+
+public class Cappuccino implements Drink {
+    private int amount;
+    private static final double BASE_PRICE = 3.00;
+
+    public Cappuccino(Cup cup) {
+        this.amount = 180;
+    }
+
+    @Override
+    public String getName() {
+        return "Cappuccino";
+    }
+
+    @Override
+    public double getPrice() {
+        return BASE_PRICE;
+    }
+
+    @Override
+    public int getAmount() {
+        return this.amount;
+    }
+}
